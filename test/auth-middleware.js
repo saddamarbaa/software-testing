f7a97e9f957721fa58811d47defab66f9ef78ce6
saddamarbaa <saddamarbaas@gamil.com>
2023-01-24 14:59:03 +0700

    test: test api with mocha and chai

---
+++ test/auth-middleware.js
@@ -0,0 +1,51 @@
+const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
+
+const authMiddleware = require('../middleware/is-auth');
+
+describe('Auth middleware', function() {
+  it('should throw an error if no authorization header is present', function() {
+    const req = {
+      get: function(headerName) {
+        return null;
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not authenticated.'
+    );
+  });
+
+  it('should throw an error if the authorization header is only one string', function() {
+    const req = {
+      get: function(headerName) {
+        return 'xyz';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function() {
+    const req = {
+      get: function(headerName) {
+        return 'Bearer djfkalsdjfaslfjdlas';
+      }
+    };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'abc' });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
+
+  it('should throw an error if the token cannot be verified', function() {
+    const req = {
+      get: function(headerName) {
+        return 'Bearer xyz';
+      }
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+});
---
+++ test/feed-controller.js
@@ -0,0 +1,67 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+const FeedController = require('../controllers/feed');
+
+describe('Feed Controller', function() {
+  before(function(done) {
+    mongoose
+      .connect(
+        'mongodb+srv://maximilian:fmFLrH6d0DjMxWcg@cluster0-ntrwp.mongodb.net/test-messages?retryWrites=true'
+      )
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a'
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  beforeEach(function() {});
+
+  afterEach(function() {});
+
+  it('should add a created post to the posts of the creator', function(done) {
+    const req = {
+      body: {
+        title: 'Test Post',
+        content: 'A Test Post'
+      },
+      file: {
+        path: 'abc'
+      },
+      userId: '5c0f66b979af55031b34728a'
+    };
+    const res = {
+      status: function() {
+        return this;
+      },
+      json: function() {}
+    };
+
+    FeedController.createPost(req, res, () => {}).then(savedUser => {
+      expect(savedUser).to.have.property('posts');
+      expect(savedUser.posts).to.have.length(1);
+      done();
+    });
+  });
+
+  after(function(done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
